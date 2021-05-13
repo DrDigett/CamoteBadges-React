65f157011276698e55d91828a7f61b410dcb7768
Diego <d9730707@gmail.com>
2021-05-13 12:39:04 -0500

    edit navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ class Navbar extends React.Component{
         return(
             <div className="Navbar">
                 <div className="container-fluid">
-                    <Link className="Navbar__brand" to="/">
+                    <Link className="Navbar__brand" to="/CamoteBadges-React">
                         <span className="font-weight-light">Camote</span>
                         <span className="font-weight-bold">Sports</span>
                     </Link>
